c074da4b57275a201283ebc1574c157a2d6795f5
Marcus Karlsson <marcus.n.o.karlsson@gmail.com>
2021-12-01 19:52:25 +0100

    Clean day 1

--- day1.py
+++ day1.py
@@ -1,17 +1,9 @@
 from numpy import convolve, ones
 
 
-def parse_input(filename):
-    with open(filename) as file:
-        return [int(line) for line in file.readlines()]
-
-
-measurements = parse_input("input/day1.in")
-
+measurements = [int(line) for line in open("input/day1.in").readlines()]
 count = sum(second > first for (first, second) in zip(measurements, measurements[1:]))
 print(f"Part 1: {count}")
-
-
 window_sums = convolve(measurements, ones(3), "valid")
 count = sum(second > first for (first, second) in zip(window_sums, window_sums[1:]))
 print(f"Part 2: {count}")
