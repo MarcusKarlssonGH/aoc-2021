0072d1e547f2154bee769e32f588ba9985573692
Marcus Karlsson <marcus.n.o.karlsson@gmail.com>
2021-12-05 18:10:55 +0100

    Remove numpy from day 1

--- day1.py
+++ day1.py
@@ -1,9 +1,6 @@
-from numpy import convolve, ones
-
-
-measurements = [int(line) for line in open("input/day1.in").readlines()]
-count = sum(second > first for (first, second) in zip(measurements, measurements[1:]))
+depths = [int(line) for line in open("input/day1.in").readlines()]
+count = sum(second > first for (first, second) in zip(depths, depths[1:]))
 print(f"Part 1: {count}")
-window_sums = convolve(measurements, ones(3), "valid")
+window_sums = [a + b + c  for a, b, c in zip(depths, depths[1:], depths[2:])]
 count = sum(second > first for (first, second) in zip(window_sums, window_sums[1:]))
 print(f"Part 2: {count}")
