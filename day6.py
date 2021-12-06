1428ca1796efdb453834da7fe647165006d62681
Marcus Karlsson <marcus.n.o.karlsson@gmail.com>
2021-12-06 07:02:58 +0100

    clean day 6

--- day6.py
+++ day6.py
@@ -4,7 +4,7 @@ def count_fish(state, days):
         reborn, *counts = counts
         counts.append(reborn)
         counts[6] += reborn
-    return sum(c for c in counts)
+    return sum(counts)
 
 state = [int(x) for x in open("input/day6.in").read().strip().split(",")]
 print(f"Part 1: {count_fish(state, 80)}")
