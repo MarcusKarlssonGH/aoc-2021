40b181ec58da6673c22a3f8ef9343c964d65cd69
Marcus Karlsson <marcus.n.o.karlsson@gmail.com>
2021-12-12 12:20:34 +0100

    solve part 2

--- day12.py
+++ day12.py
@@ -17,31 +17,39 @@ def is_small(cave):
 
 connections = parse_tree("input/day12.in")
 # print(connections)
-paths = [['start']]
 
-while True:
-    all_new_paths = []
-    unfinished_paths = [p for p in paths if p[-1] != 'end']
-    finished_paths = [p for p in paths if p[-1] == 'end']
-    for path in unfinished_paths:
-        last = path[-1]
-        neighbors = connections[last]
-        to_add = [n for n in neighbors if not (is_small(n) and n in path)]
-        for n in to_add:
-            tmp = list(path)
-            tmp.append(n)
-            all_new_paths.append(tmp)
+def get_paths(connections, special_cave):
+    paths = [['start']]
+    while True:
+        all_new_paths = []
+        unfinished_paths = [p for p in paths if p[-1] != 'end']
+        finished_paths = [p for p in paths if p[-1] == 'end']
+        for path in unfinished_paths:
+            last = path[-1]
+            neighbors = connections[last]
+            to_add = [n for n in neighbors if not (is_small(n) and n in path)]
+            if special_cave in neighbors and path.count(special_cave) == 1:
+                to_add.append(special_cave)
+            for n in to_add:
+                tmp = list(path)
+                tmp.append(n)
+                all_new_paths.append(tmp)
 
-    paths = list(finished_paths) + list(all_new_paths)
-    if all(p[-1] == 'end' for p in paths):
-        break
+        paths = list(finished_paths) + list(all_new_paths)
+        if all(p[-1] == 'end' for p in paths):
+            break
+    return paths
+
+
+small_caves = [c for c in connections if (is_small(c) and c != 'start' and c != 'end')]
+
+all_paths = set()
+for c in small_caves:
+    paths = get_paths(connections, c)
+    for p in paths:
+        all_paths.add("-".join(p))
+    print(len(all_paths))
+
+    # print(all_paths)
     # print(paths)
-    # print(len(paths))
-    # input()
-    # print(paths)
-    # input()
-# print(paths)
-print(len(paths))
-
-
 
